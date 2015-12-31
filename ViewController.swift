53b75bfcb30dab216fae0581f32415dd3e6fa755
haowu1 <haowu677@yahoo.com>
2015-12-31 17:50:20 -0500

    Tipper main code
    The work that I did

---
+++ ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Hao Wu  on 12/28/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+
+    override func viewDidLoad() {2
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    tipLabel.text = "$0.00"
+    totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.10, 0.15, 0.20]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing (true)
+    }
+}
+
